541843573817e3d982c520ea1fc2fffbe9d7d349
hulilei <hulilei@jiagouyun.com>
2021-08-05 19:00:31 +0800

    FTTrackDataManger 类添加

--- FTMacOSSDK/Manger/FTTrackDataManger.h
+++ FTMacOSSDK/Manger/FTTrackDataManger.h
@@ -7,11 +7,27 @@
 //
 
 #import <Foundation/Foundation.h>
-
+/**
+ - FTAddDataConcurrent: 异步并行写入数据库
+ - FTAddDataSerial: 异步串行写入数据库
+ - FTAddDataCache:  事务写入数据库
+ - FTAddDataImmediate: 同步写入数据库
+ */
+typedef NS_ENUM(NSInteger, FTAddDataType) {
+    FTAddDataConcurrent,
+    FTAddDataSerial,
+    FTAddDataCache,
+    FTAddDataImmediate,
+};
 NS_ASSUME_NONNULL_BEGIN
-
+@class FTRecordModel;
+///数据写入，数据上传 相关操作
 @interface FTTrackDataManger : NSObject
+///数据写入
+- (void)addTrackData:(FTRecordModel *)data type:(FTAddDataType)type;
 
+///上传数据
+- (void)uploadTrackData;
 @end
 
 NS_ASSUME_NONNULL_END
--- FTMacOSSDK/Manger/FTTrackDataManger.m
+++ FTMacOSSDK/Manger/FTTrackDataManger.m
@@ -7,7 +7,86 @@
 //
 
 #import "FTTrackDataManger.h"
+#import "FTRecordModel.h"
+#import "FTReachability.h"
+#import "FTTrackerEventDBTool.h"
+@interface FTTrackDataManger ()
+@property (nonatomic, strong) FTReachability *reachability;
+@property (nonatomic, strong) dispatch_queue_t serialQueue;
+@property (nonatomic, strong) dispatch_queue_t concurrentLabel;
+@property (nonatomic, assign) BOOL isUploading;
+@end
+@implementation FTTrackDataManger{
+    dispatch_semaphore_t _lock;
+}
++(instancetype)sharedInstance{
+    static  FTTrackDataManger *sharedInstance;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[super allocWithZone:nil] init];
+    });
+    return sharedInstance;
+}
 
-@implementation FTTrackDataManger
++(instancetype)allocWithZone:(struct _NSZone *)zone{
+    return [FTTrackDataManger sharedInstance];
+}
+- (id)copyWithZone:(NSZone *)zone {
+    return self;
+}
 
+- (id)mutableCopyWithZone:(NSZone *)zone {
+    return self;
+}
+-(instancetype)init{
+    self = [super init];
+    if (self) {
+        self.serialQueue = dispatch_queue_create("dataflux_track_data_serial", DISPATCH_QUEUE_SERIAL);
+        self.concurrentLabel = dispatch_queue_create("dataflux_track_data_concurrent", DISPATCH_QUEUE_CONCURRENT);
+        _lock = dispatch_semaphore_create(1);
+
+    }
+    return self;
+}
+- (void)addTrackData:(FTRecordModel *)data type:(FTAddDataType)type{
+    switch (type) {
+        case FTAddDataConcurrent:{
+            dispatch_async(self.concurrentLabel, ^{
+                [[FTTrackerEventDBTool sharedManger] insertItemWithItemData:data];
+            });
+            break;
+        }
+        case FTAddDataSerial:{
+            dispatch_async(self.serialQueue, ^{
+                [[FTTrackerEventDBTool sharedManger] insertItemWithItemData:data];
+            });
+            break;
+        }
+        case FTAddDataCache:{
+            dispatch_async(self.concurrentLabel, ^{
+                [[FTTrackerEventDBTool sharedManger] insertItemToCache:data];
+            });
+            break;
+        }
+        case FTAddDataImmediate:{
+            [[FTTrackerEventDBTool sharedManger] insertItemToCache:data];
+            break;
+        }
+    }
+    
+}
+
+
+- (void)uploadTrackData{
+    if (self.isUploading) {
+        return;
+    }
+    dispatch_semaphore_wait(_lock, DISPATCH_TIME_FOREVER);
+    self.isUploading = YES;
+    dispatch_semaphore_signal(_lock);
+    
+    
+    
+    
+}
 @end
