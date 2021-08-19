b804e399788049e2c5470c5815ac7aa7f008aa15
hulilei <hulilei@jiagouyun.com>
2021-08-19 18:57:12 +0800

    上传逻辑测试用例添加

--- FTMacOSSDKTests/FTMacOSSDKTests.m
+++ FTMacOSSDKTests/FTMacOSSDKTests.m
@@ -7,6 +7,14 @@
 
 #import <XCTest/XCTest.h>
 #import <Utils/FTDateUtil.h>
+#import <FTRecordModel.h>
+#import <Utils/FTConstants.h>
+#import "FTTrackDataManger.h"
+#import "FTRequest.h"
+#import "FTNetworkManager.h"
+#import <FTTrackConfig.h>
+#import <FTSDKAgent.h>
+#import "FTTrackerEventDBTool.h"
 @interface FTMacOSSDKTests : XCTestCase
 
 @end
@@ -15,6 +23,10 @@
 
 - (void)setUp {
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+    NSString *url = [processInfo environment][@"ACCESS_SERVER_URL"];
+    FTTrackConfig *config = [[FTTrackConfig alloc]initWithMetricsUrl:url];
+    [FTSDKAgent startWithConfigOptions:config];
 }
 
 - (void)tearDown {
@@ -23,7 +35,29 @@
 - (void)testCurrentDate{
    long long time = [FTDateUtil currentTimeMillisecond];
 }
-- (void)testExample {
+
+- (void)testEventUpload{
+    XCTestExpectation *expectation= [self expectationWithDescription:@"异步操作timeout"];
+    FTRecordModel *model = [[FTRecordModel alloc]initWithSource:@"testUploading" op:FTDataTypeLOGGING tags:@{@"name":@"testEventUpload1"} field:@{@"event":@"testEventUpload"} tm:[FTDateUtil currentTimeNanosecond]];
+    NSInteger oldCount = [[FTTrackerEventDBTool sharedManger] getDatasCount];
+    
+    [[FTTrackDataManger sharedInstance] addTrackData:model type:FTAddDataNormal];
+    [NSThread sleepForTimeInterval:2];
+    NSInteger newCount = [[FTTrackerEventDBTool sharedManger] getDatasCount];
+    XCTAssertTrue(newCount>oldCount);
+    [NSThread sleepForTimeInterval:10];
+    FTRecordModel *model2 = [[FTRecordModel alloc]initWithSource:@"testUploading" op:FTDataTypeLOGGING tags:@{@"name":@"testEventUpload2"} field:@{@"event":@"testEventUpload"} tm:[FTDateUtil currentTimeNanosecond]];
+    [[FTTrackDataManger sharedInstance] addTrackData:model2 type:FTAddDataNormal];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSInteger uploadedCount = [[FTTrackerEventDBTool sharedManger] getDatasCount];
+        XCTAssertTrue(uploadedCount == 0);
+        [expectation fulfill];
+    });
+    
+    [self waitForExpectationsWithTimeout:30 handler:^(NSError *error) {
+        XCTAssertNil(error);
+    }];
+    
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
 }
---
+++ FTMacOSSDKTests/FTNetworkTests.m
@@ -0,0 +1,57 @@
+//
+//  FTNetworkTests.m
+//  FTMacOSSDKTests
+//
+//  Created by 胡蕾蕾 on 2021/8/19.
+//  Copyright © 2021 DataFlux-cn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import <Utils/FTDateUtil.h>
+#import <FTRecordModel.h>
+#import <Utils/FTConstants.h>
+#import "FTTrackDataManger.h"
+#import "FTRequest.h"
+#import "FTNetworkManager.h"
+#import <FTTrackConfig.h>
+#import <FTSDKAgent.h>
+@interface FTNetworkTests : XCTestCase
+
+@end
+
+@implementation FTNetworkTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+    NSString *url = [processInfo environment][@"ACCESS_SERVER_URL"];
+    FTTrackConfig *config = [[FTTrackConfig alloc]initWithMetricsUrl:url];
+    [FTSDKAgent startWithConfigOptions:config];
+}
+
+- (void)testLoggingUpload{
+    XCTestExpectation *expectation= [self expectationWithDescription:@"异步操作timeout"];
+
+    FTRecordModel *model = [[FTRecordModel alloc]initWithSource:@"testUploading" op:FTDataTypeLOGGING tags:@{@"name":@"testUpload"} field:@{@"event":@"testUpload"} tm:[FTDateUtil currentTimeNanosecond]];
+    FTRequest *request = [[FTRequest alloc]initWithEvents:@[model] type:FTDataTypeLOGGING];
+    [[FTNetworkManager sharedInstance] sendRequest:request completion:^(NSHTTPURLResponse * _Nonnull httpResponse, NSData * _Nullable data, NSError * _Nullable error) {
+        XCTAssertTrue(httpResponse.statusCode == 200);
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:30 handler:^(NSError *error) {
+        XCTAssertNil(error);
+    }];
+}
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
