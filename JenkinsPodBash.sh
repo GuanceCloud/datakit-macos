71fc8bb60f6198265de85f075e5cd05f1e1edce5
章博 <brandonfaustine@gmail.com>
2023-05-19 12:15:50 +0800

    优化脚本逻辑

--- JenkinsPodBash.sh
+++ JenkinsPodBash.sh
@@ -1,9 +1,9 @@
-#FT_PUSH_TAG="refs/tags/1.0.2-alpha.8"
+FT_PUSH_TAG="refs/tags/1.0.0-alpha.1"
 
 
 VERSION=$(echo "$FT_PUSH_TAG" | sed -e 's/.*\///g' | sed -e 's/~.*//g' )
 
-git config remote.github.url >&- || git remote add github git@github.com:GuanceCloud/datakit-macos.git
+if git config remote.github.url; then git config remote.github.url git@github.com:GuanceCloud/datakit-macos.git; else git remote add github git@github.com:GuanceCloud/datakit-macos.git; fi
 git push github $VERSION
 
 if [[ $? -eq 0 ]];then
@@ -12,7 +12,7 @@ if [[ $? -eq 0 ]];then
 
   sed  -i '' 's/$JENKINS_DYNAMIC_VERSION/'"$VERSION"'/g' FTMacOSSDK.podspec
 
-  pod trunk push FTMacOSSDK.podspec --verbose --allow-warnings
+  echo pod trunk push FTMacOSSDK.podspec --verbose --allow-warnings
 
 else
   exit  1
