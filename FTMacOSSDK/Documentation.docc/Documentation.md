45efc3a50b039146876595a2573a0bafbe25d8a3
hulilei <huuuu1016@gmail.com>
2023-06-25 17:43:36 +0800

    添加支持查看文档覆盖率

---
+++ FTMacOSSDK/Documentation.docc/Documentation.md
@@ -0,0 +1,17 @@
+# ``FTMacOSSDK``
+
+用于收集 macOS 应用的指标数据，用于 “观测云” 应用监测功能，以可视化的方式分析各个 macOS 应用端的性能。
+
+## Topics
+
+
+### Enumerations
+- ``FTMacOSSDK/FTEnv``
+- ``FTMacOSSDK/FTLogStatus``
+- ``FTMacOSSDK/FTMonitorFrequency``
+- ``FTMacOSSDK/FTErrorMonitorType``
+- ``FTMacOSSDK/FTNetworkTraceType``
+- ``FTMacOSSDK/FTDeviceMetricsMonitorType``
+- ``FTMacOSSDK/FTLogCacheDiscard``
+
+
---
+++ doccCoverage.sh
@@ -0,0 +1,63 @@
+# sh doccCoverage.sh all  获取全部文档覆盖率
+# sh doccCoverage.sh      获取 public 文件 文档覆盖率
+project_path='FTMacOSSDK.xcodeproj/project.pbxproj'
+sdk_path=$(pwd)/build
+
+changeFileAttributeToPublic(){
+states='init'
+lineNum=0
+cat -n $project_path | while read line
+do
+  lineNum=`expr $lineNum + 1`
+  if [[ $line =~ 'Begin PBXBuildFile section */' ]]
+  then
+  states='start'
+      echo $line
+  elif [[ $line =~ 'End PBXBuildFile section */' ]]
+  then
+  states='end'
+      echo $line
+  elif [[ $states = 'start' ]]
+  then
+    if [[ $line =~ '.h in Headers */' ]];then
+       if [[ $line =~ 'settings = {ATTRIBUTES = (Public,' ]];then
+           echo "已经是 Public 文件"
+       else
+           #brew install gnu-sed
+           sed -i "" "$lineNum s/\}\;/settings = \{ATTRIBUTES = \(Public, \)\; \}\; \}\;/" $project_path
+       fi
+    fi
+  fi
+done
+}
+
+doccCoverage(){
+xcodebuild -target FTMacOSSDK DOCC_EXTRACT_SWIFT_INFO_FOR_OBJC_SYMBOLS=NO
+
+xcrun docc convert FTMacOSSDK/Documentation.docc \
+--fallback-display-name FTMacOSSDK \
+--fallback-bundle-identifier com.cloudcare.ft.mobile.sdk.FTMacOSSDK \
+--fallback-bundle-version 1.0 \
+--additional-symbol-graph-dir ./build/FTMacOSSDK.build/Release/FTMacOSSDK.build/symbol-graph \
+--experimental-documentation-coverage \
+--level detailed
+}
+
+# 若为 all 则获取所有文件的注释覆盖率
+FT_ALL_FILE_COVERAGE="$1"
+echo "----- Start -----"
+
+if [[ "$FT_ALL_FILE_COVERAGE" == "all" ]]; then
+echo "-----changeFileAttributeToPublic Start-----"
+changeFileAttributeToPublic
+echo "-----changeFileAttributeToPublic End-----"
+fi
+
+echo "-----Coverage Start-----"
+doccCoverage
+echo "-----Coverage End-----"
+
+if [[ "$FT_ALL_FILE_COVERAGE" == "all" ]]; then
+git checkout -- $project_path
+echo "----- End -----"
+fi
