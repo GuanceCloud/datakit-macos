3e4a23f8a203f905082a1425b714acb4180bd6b5
hulilei <huuuu1016@gmail.com>
2023-08-17 18:50:33 +0800

    example webview 更新

--- Example/Example/TabView/WebViewController.m
+++ Example/Example/TabView/WebViewController.m
@@ -8,7 +8,7 @@
 #import "WebViewController.h"
 #import <WebKit/WebKit.h>
 
-@interface WebViewController ()
+@interface WebViewController ()<WKUIDelegate>
 @property (nonatomic, strong) WKWebView *mWebView;
 
 @end
@@ -33,11 +33,21 @@
     WKWebViewConfiguration *config = [[WKWebViewConfiguration alloc] init];
     config.userContentController = userContentController;
     self.mWebView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:config];
+    self.mWebView.UIDelegate = self;
     [self.view addSubview:self.mWebView];
-    NSURL *url =  [[NSBundle mainBundle] URLForResource:@"sample" withExtension:@"html"];
+    NSProcessInfo *processInfo = [NSProcessInfo processInfo];
+    NSString *urlStr = [processInfo environment][@"WEB_URL"];
+    NSURL *url =  [NSURL URLWithString:[NSString stringWithFormat:@"%@?requestUrl=%@/api/user",urlStr,urlStr]];
 
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
     [self.mWebView loadRequest:request];
 }
-
+-(void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler{
+    NSAlert *alert = [[NSAlert alloc]init];
+    [alert addButtonWithTitle:@"ok"];
+    [alert setMessageText:message];
+    [alert setAlertStyle:NSAlertStyleInformational];
+    [alert beginSheetModalForWindow:self.view.window completionHandler:nil];
+    completionHandler();
+}
 @end
